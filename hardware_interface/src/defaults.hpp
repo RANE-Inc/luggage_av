2265ec2e05bf92bfb4f79e5e7cbcb30ead574189
Fufs <rafalfufs.rytwinski@gmail.com>
2024-11-04 17:45:50 -0500

    merge luggage_av_hardware_interface into luggage_av

---
+++ hardware_interface/src/defaults.hpp
@@ -0,0 +1,13 @@
+#include <cstdint>
+
+namespace luggage_av {
+
+    struct {
+        char* dev = const_cast<char*>("/dev/ttyACM0");
+        double lin_vel_min = -1.0;
+        double lin_vel_max = 1.0;
+        int32_t hw_cmd_min = -8312;
+        int32_t hw_cmd_max = 8312;
+    } luggage_av_default_parameters;
+
+}  // namespace luggage_av
