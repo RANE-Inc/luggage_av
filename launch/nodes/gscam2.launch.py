a076b60520fd002a0ad0fe7dc024352522c003b3
Fufs <42976807+Fufs@users.noreply.github.com>
2025-03-05 19:30:43 -0500

    Add gscam2 launch file
    TODO: Finalize python restreamer/Move to Node
    TODO: RTP?
    TODO: Calibration
    TODO: URDF
    TODO: RVIZ
    TODO: Integrate in other launch files
    TODO: GZ
    TODO: Frontend

---
+++ launch/nodes/gscam2.launch.py
@@ -0,0 +1,47 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
+
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+
+    pkg_share = get_package_share_directory('luggage_av')
+
+    camera_name = LaunchConfiguration("camera_name")
+    namespace = LaunchConfiguration("namespace")
+
+    gscam2_node = Node(
+        package='gscam2',
+        executable='gscam_main',
+        name='gscam_publisher',
+        output='screen',
+        parameters=[
+            os.path.join(pkg_share, "parameters", "gscam2.yaml"),
+            {
+                "camera_name": camera_name,
+                "camera_info_url": ["file://", PathJoinSubstitution([pkg_share, "configs", camera_name]),".ini"],
+            }
+        ],
+        namespace=["/", namespace],
+    )
+
+
+    return LaunchDescription([
+        DeclareLaunchArgument(
+            'camera_name',
+            default_value="logitech_c921",
+            description="The name of the camera. Note: Do not include whitespaces in your name"
+        ),
+        DeclareLaunchArgument(
+            'namespace',
+            default_value="luggage_av",
+            description="Namespace of the bot (usually its unique identifier)"
+        ),
+
+        gscam2_node
+    ])
