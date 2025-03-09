c596d0f5b0f7e8394ce014152708a2986588b7d9
EricEaton99 <wring8@gmail.com>
2025-03-09 11:57:29 -0400

    removed dependency on Nav2 and reduced launch args

--- launch/rosbridge.launch.py
+++ launch/rosbridge.launch.py
@@ -3,10 +3,7 @@ from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
 from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
-from launch.conditions import IfCondition, UnlessCondition
 import os
-from nav2_common.launch import RewrittenYaml
-from launch_ros.parameter_descriptions import ParameterFile
 
 def generate_launch_description():
     # Get the package share directory
@@ -14,55 +11,30 @@ def generate_launch_description():
     params_file_full_path = os.path.join(pkg_share, 'parameters', 'rosbridge_params.yaml')
 
     namespace = LaunchConfiguration('namespace')
-    ssl = LaunchConfiguration('ssl')
     param_substitutions = {
         'rosbridge_websocket.ros__parameters.port': LaunchConfiguration('port'),
         'rosbridge_websocket.ros__parameters.address': LaunchConfiguration('address'),
-        'rosbridge_websocket.ros__parameters.ssl': ssl,
         'rosbridge_websocket.ros__parameters.certfile': LaunchConfiguration('certfile'),
         'rosbridge_websocket.ros__parameters.keyfile': LaunchConfiguration('keyfile'),
         # Glob patterns for filtering and other Launch Configs can be exposed
     }
 
-    # Process parameters with substitutions and namespace
-    configured_params = ParameterFile(
-        RewrittenYaml(
-            source_file=LaunchConfiguration('params_file'),
-            root_key=namespace,
-            param_rewrites=param_substitutions,
-            convert_types=True
-        ),
-        allow_substs=True
-    )
-
     # Define launch arguments
     args = [
-        DeclareLaunchArgument('namespace', default_value='', description='Top-level namespace'),
-        DeclareLaunchArgument('params_file', default_value=params_file_full_path, description='Full path to the ROS2 parameters file for rosbridge'),
+        DeclareLaunchArgument('namespace', default_value='luggage_av', description='Top-level namespace'),
         DeclareLaunchArgument('port', default_value='9090'),
         DeclareLaunchArgument('address', default_value=''),
-        DeclareLaunchArgument('ssl', default_value='false'),
         DeclareLaunchArgument('certfile', default_value=''),
         DeclareLaunchArgument('keyfile', default_value=''),
     ]
 
     # Define nodes
-    ssl_node = Node(
+    rosbrigde_node = Node(
         package='rosbridge_server',
         executable='rosbridge_websocket',
         namespace=namespace,
         output='screen',
-        parameters=[configured_params],
-        condition=IfCondition(ssl)
-    )
-
-    non_ssl_node = Node(
-        package='rosbridge_server',
-        executable='rosbridge_websocket',
-        namespace=namespace,
-        output='screen',
-        parameters=[configured_params],
-        condition=UnlessCondition(ssl)
+        parameters=[params_file_full_path, param_substitutions],
     )
 
     rosapi_node = Node(
@@ -70,7 +42,7 @@ def generate_launch_description():
         executable='rosapi_node',
         namespace=namespace,
         output='screen',
-        parameters=[configured_params]
+        parameters=[params_file_full_path, param_substitutions],
     )
 
-    return LaunchDescription(args + [ssl_node, non_ssl_node, rosapi_node])
+    return LaunchDescription(args + [rosbrigde_node, rosapi_node])
