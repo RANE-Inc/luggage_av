566498328f023cf046cbaaaeea4892b93b9ae691
Fufs <42976807+Fufs@users.noreply.github.com>
2025-03-09 23:09:01 -0400

    Minor Fixes

--- launch/nodes/rosbridge.launch.py
+++ launch/nodes/rosbridge.launch.py
@@ -1,48 +1,53 @@
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument
+from launch.actions import DeclareLaunchArgument, GroupAction
 from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
 import os
 
+
 def generate_launch_description():
+
     # Get the package share directory
     pkg_share = get_package_share_directory('luggage_av')  # Replace with your package name
     params_file_full_path = os.path.join(pkg_share, 'parameters', 'rosbridge_params.yaml')
 
     namespace = LaunchConfiguration('namespace')
-    param_substitutions = {
-        'rosbridge_websocket.ros__parameters.port': LaunchConfiguration('port'),
-        'rosbridge_websocket.ros__parameters.address': LaunchConfiguration('address'),
-        'rosbridge_websocket.ros__parameters.certfile': LaunchConfiguration('certfile'),
-        'rosbridge_websocket.ros__parameters.keyfile': LaunchConfiguration('keyfile'),
-        # Glob patterns for filtering and other Launch Configs can be exposed
-    }
 
     # Define launch arguments
-    args = [
+    args = GroupAction([
         DeclareLaunchArgument('namespace', default_value='luggage_av', description='Top-level namespace'),
         DeclareLaunchArgument('port', default_value='9090'),
         DeclareLaunchArgument('address', default_value=''),
-        DeclareLaunchArgument('certfile', default_value=''),
-        DeclareLaunchArgument('keyfile', default_value=''),
-    ]
+    ])
 
     # Define nodes
     rosbrigde_node = Node(
         package='rosbridge_server',
         executable='rosbridge_websocket',
-        namespace=namespace,
         output='screen',
-        parameters=[params_file_full_path, param_substitutions],
+        parameters=[
+            params_file_full_path,
+            {
+                'port': LaunchConfiguration('port'),
+                'address': LaunchConfiguration('address'),
+            },
+        ],
+        namespace=namespace,
     )
 
     rosapi_node = Node(
         package='rosapi',
         executable='rosapi_node',
-        namespace=namespace,
         output='screen',
-        parameters=[params_file_full_path, param_substitutions],
+        parameters=[
+            params_file_full_path,
+        ],
+        namespace=namespace,
     )
 
-    return LaunchDescription(args + [rosbrigde_node, rosapi_node])
+    return LaunchDescription([
+        args,
+        rosbrigde_node,
+        rosapi_node
+    ])
