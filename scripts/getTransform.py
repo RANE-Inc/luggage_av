7cf7985467a6cd0a59a73ebabcf36f90b038b0c9
EricEaton99 <wring8@gmail.com>
2025-03-11 09:50:15 -0400

    added publisher and cleaned up code

--- scripts/getTransform.py
+++ scripts/getTransform.py
@@ -5,50 +5,49 @@ from tf2_ros import TransformException
 from tf2_ros.buffer import Buffer
 from tf2_ros.transform_listener import TransformListener
 from geometry_msgs.msg import TransformStamped
-from geometry_msgs.msg import Twist
-from rclpy.qos import QoSProfile
-from tf2_ros import LookupException
-import sys
-import math
 
 class BaseLinkToMapListener(Node):
 
     def __init__(self, first_frame='base_link', second_frame='map'):
-        super().__init__('base_link_to_map_listener')
+        super().__init__('map_to_base_link_listener')
+        
+        # setup the frames
         namespace = self.get_namespace().lstrip('/')
-        self.first_name_ = f'{namespace}/' + first_frame
-        self.second_name_ = f'{namespace}/' + second_frame
+        self.base_frame_ = f'{namespace}/' + first_frame
+        self.target_frame_ = f'{namespace}/' + second_frame
+        
+        # setup the publisher
+        self.publisher_ = self.create_publisher(TransformStamped, f'{self.base_frame_}_to_{self.target_frame_}_transform', 10)
+
+        # setup the tf listener
         self.tf_buffer = Buffer()
         self.tf_listener = TransformListener(self.tf_buffer, self)
-        # self.cmd_ = Twist ()
-        # self.publisher_ = self.create_publisher(Twist, "{}/cmd_vel".format(self.second_name_),10)
-        
-        self.timer = self.create_timer(1.0, self.timer_callback)
+        self.timer = self.create_timer(0.5, self.timer_callback)
 
     def timer_callback(self):
         try:
-            # Lookup the transform from map to base_link
+            # Lookup the transform
             transform: TransformStamped = self.tf_buffer.lookup_transform(
-                self.first_name_,
-                self.second_name_,
+                self.base_frame_,
+                self.target_frame_,
                 rclpy.time.Time()  # Time (0 means latest available)
             )
+            # Publish the transform
+            self.publisher_.publish(transform)
             self.print_transform(transform)
-            self.timer.cancel()
-            # TODO: figure out how to end this properly and not just cancel the timer which results in hanging program
 
         except TransformException as ex:
             self.get_logger().info(f"Could not get transform: {ex}")
 
     def print_transform(self, transform: TransformStamped):
-        # self.get_logger().info(f"Transform: {transform}")
-        self.get_logger().info(f"Translation: [x: {transform.transform.translation.x}, "
-                              f"y: {transform.transform.translation.y}, "
-                              f"z: {transform.transform.translation.z}]")
-        self.get_logger().info(f"Rotation: [x: {transform.transform.rotation.x}, "
-                              f"y: {transform.transform.rotation.y}, "
-                              f"z: {transform.transform.rotation.z}, "
-                              f"w: {transform.transform.rotation.w}]")
+        self.get_logger().info(f"Publishing tf: "
+                    f"[x: {transform.transform.translation.x:.3f}, "
+                    f"y: {transform.transform.translation.y:.3f}, "
+                    f"z: {transform.transform.translation.z:.3f}]"
+                    f", [x: {transform.transform.rotation.x:.3f}, "
+                    f"y: {transform.transform.rotation.y:.3f}, "
+                    f"z: {transform.transform.rotation.z:.3f}, "
+                    f"w: {transform.transform.rotation.w:.3f}]")
 
 def main(args=None):
     rclpy.init(args=args)
@@ -59,7 +58,8 @@ def main(args=None):
         pass
     finally:
         node.destroy_node()
-        rclpy.shutdown()
+        if rclpy.ok():
+            rclpy.shutdown()
 
 if __name__ == '__main__':
     main()
